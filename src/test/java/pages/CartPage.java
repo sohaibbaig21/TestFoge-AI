d548a75c5799ede8855f5e50dc2c88e4f6071136
sohaibbaig21 <sohaibbaig000@gmail.com>
2026-04-20 02:50:49 +0500

    updated cartPage to handle race condition

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -4,13 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class CartPage {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
+        // Initialize the wait with a 10-second timeout
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         PageFactory.initElements(driver, this);
     }
 
@@ -18,6 +24,10 @@ public class CartPage {
     WebElement checkoutBtn;
 
     public void clickCheckout() {
-        checkoutBtn.click();
+        // 1. Wait for the URL to contain 'cart' to ensure we are on the right page
+        wait.until(ExpectedConditions.urlContains("cart.html"));
+
+        // 2. Wait for the button to be visible and clickable
+        wait.until(ExpectedConditions.elementToBeClickable(checkoutBtn)).click();
     }
 }
